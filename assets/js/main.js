70e566117aeff14439412f98371dec6209600fb8
Gavin Barnett <gavbarnett@gmail.com>
2017-06-22 15:36:13 +0100

    added basic canvas display
    I don't like it.
    Having ideas of more fluid to less fluid structure (pin board, mind map,
    list....bash style plain text interface). Can't decide!!

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,24 @@
 //but it's only a test, :-D
 var myAPI = "2tm9Xv3INUU5GH_HfSB2V_jelm89by6d"
 var urlbase = "https://api.mlab.com/api/1/databases/ideadb/collections/test"
+var objecttracker = [];
 //on load do:
 $(document).ready(function() {
   var title = Math.floor(Math.random() * 100);
   var description = Math.floor(Math.random() * 1000);
-  add_data(title, description);
-  get_data();
+  //add_data(title, description);
+  load_canvas();
 });
 
-function get_data() {
+function load_canvas() {
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+  c.width = window.innerWidth;
+  c.height = window.innerHeight;
+  get_data(1);
+}
+
+function get_data(draw) {
   $.ajax({
     url: urlbase + "?apiKey=" + myAPI,
   }).done(function(data) {
@@ -18,6 +27,28 @@ function get_data() {
     $.each(data, function(key, data) {
       console.log(data.title, data._id.$oid);
     });
+    if (draw) {
+      var c = document.getElementById("myCanvas");
+      var ctx = c.getContext("2d");
+      ctx.clearRect(0, 0, c.width, c.height);
+      var counter = 0;
+      $.each(data, function(key, data) {
+        counter += 1;
+        objecttracker["id"] = data._id.$oid
+        objecttracker["x"] = (20)
+        objecttracker["y"] = (counter * 50)
+        ctx.fillStyle = "#0000FF";
+        ctx.fillRect(objecttracker["x"], objecttracker["y"], 400, 30);
+        ctx.font = "22px Arial";
+        ctx.fillStyle = "#ffffff";
+        ctx.fillText(data.title, objecttracker["x"] + 10, objecttracker["y"] + 25);
+        ctx.fillStyle = "#eeeeee";
+        ctx.fillRect(objecttracker["x"], objecttracker["y"] + 30, 400, 170);
+        ctx.font = "16px Arial";
+        ctx.fillStyle = "#333333";
+        ctx.fillText(data.description, objecttracker["x"] + 10, objecttracker["y"] + 45);
+      });
+    }
   })
 }
 
