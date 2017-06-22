c6295c582ffa06761baddd933c4b7a057bcaa99e
Gavin Barnett <gavbarnett@gmail.com>
2017-06-22 16:34:25 +0100

    revert
    this is going to be text base.
    so focus is on clean console commands that can be ported directly into
    browser.

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,16 +8,8 @@ $(document).ready(function() {
   var title = Math.floor(Math.random() * 100);
   var description = Math.floor(Math.random() * 1000);
   //add_data(title, description);
-  load_canvas();
 });
 
-function load_canvas() {
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
-  c.width = window.innerWidth;
-  c.height = window.innerHeight;
-  get_data(1);
-}
 
 function get_data(draw) {
   $.ajax({
@@ -27,28 +19,6 @@ function get_data(draw) {
     $.each(data, function(key, data) {
       console.log(data.title, data._id.$oid);
     });
-    if (draw) {
-      var c = document.getElementById("myCanvas");
-      var ctx = c.getContext("2d");
-      ctx.clearRect(0, 0, c.width, c.height);
-      var counter = 0;
-      $.each(data, function(key, data) {
-        counter += 1;
-        objecttracker["id"] = data._id.$oid
-        objecttracker["x"] = (20)
-        objecttracker["y"] = (counter * 50)
-        ctx.fillStyle = "#0000FF";
-        ctx.fillRect(objecttracker["x"], objecttracker["y"], 400, 30);
-        ctx.font = "22px Arial";
-        ctx.fillStyle = "#ffffff";
-        ctx.fillText(data.title, objecttracker["x"] + 10, objecttracker["y"] + 25);
-        ctx.fillStyle = "#eeeeee";
-        ctx.fillRect(objecttracker["x"], objecttracker["y"] + 30, 400, 170);
-        ctx.font = "16px Arial";
-        ctx.fillStyle = "#333333";
-        ctx.fillText(data.description, objecttracker["x"] + 10, objecttracker["y"] + 45);
-      });
-    }
   })
 }
 
